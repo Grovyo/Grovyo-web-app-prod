10a31c4bac1eef4d36007f9dbcc7d5f6c8e3cb95
Shreyansh Singh <63132919+krishna9singh@users.noreply.github.com>
2023-10-26 16:45:09 +0530

    2

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -14,10 +13,9 @@ const firebaseConfig = {
   storageBucket: "grovyo-89dc2.appspot.com",
   messagingSenderId: "16467726056",
   appId: "1:16467726056:web:dc23db4efe27f6adaac35d",
-  measurementId: "G-E1BLGHPWL1"
+  measurementId: "G-E1BLGHPWL1",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
